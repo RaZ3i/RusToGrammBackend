d5b506fa873da900c21d45abde79913368ff5bed
RaZ3i <tony.ni2016@yandex.ru>
2025-03-15 16:46:31 +0400

    refactor UserRegister model validators

--- src/errors.py
+++ src/errors.py
@@ -2,18 +2,10 @@ from fastapi import HTTPException, status
 
 
 class Errors:
-    wrong_pass = HTTPException(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        detail={"success": False, "msg": "fail pass validation"},
+    wrong_data = HTTPException(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail={"success": False}
     )
-    wrong_login = HTTPException(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        detail={"success": False, "msg": "fail login validation"},
-    )
-    wrong_phone = HTTPException(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        detail={"success": False, "msg": "fail phone validation"},
-    )
+
     inv_token = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail={"success": False, "msg": "invalid token"},
--- src/schemas/authin.py
+++ src/schemas/authin.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel, Field, EmailStr, ConfigDict, field_validator
-from typing import Annotated
+from pydantic import (
+    BaseModel,
+    Field,
+    EmailStr,
+    ConfigDict,
+    field_validator,
+    model_validator,
+)
+from email_validator import validate_email, EmailNotValidError
+from typing import Annotated, Any
 import re
 from src.errors import Errors
 
@@ -8,39 +16,92 @@ class UserRegisterIn(BaseModel):
     # model_config = ConfigDict(regex_engine="python-re")
     login: str
     phone: str
-    email: EmailStr
+    email: str
+    # email: EmailStr
     long_hashed_password: str
     short_hashed_password: str
 
-    @field_validator("login")
+    @model_validator(mode="before")
     @classmethod
-    def login_match(cls, value: str):
-        if not re.match(
-            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{4,15}$",
-            value,
-        ):
-            raise Errors.wrong_login
-        return value
+    def check_inputs(cls, data: dict):
+        output_data = []
+        if isinstance(data, dict):
+            if not re.match(
+                r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{4,15}$",
+                data["login"],
+            ):
+                output_data.append(
+                    {
+                        "code": 99,
+                        "msg": "Логин должен содержать: от 4 до 15 символов; большие и маленькие буквы латиницы; цифры",
+                    }
+                )
+            if not re.match(
+                r"^(8|\+7)\d{10}$",
+                data["phone"],
+            ):
+                output_data.append(
+                    {
+                        "code": 96,
+                        "msg": "Пример телефона +78451254325/89651542355",
+                    }
+                )
+            if not re.match(
+                r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$",
+                data["long_hashed_password"],
+            ):
+                output_data.append(
+                    {
+                        "code": 98,
+                        "msg": "Пароль должен содержать: минимум 10 символов; большие и маленькие буквы латиницы; цифры; спецсимволы",
+                    }
+                )
+            try:
+                validate_email(data["email"])
+            except EmailNotValidError:
+                output_data.append(
+                    {
+                        "code": 97,
+                        "msg": "Email не прошел проверку. Пример правильного emailbox@mail.ru",
+                    }
+                )
+            finally:
+                if len(output_data) == 0:
+                    return data
+                else:
+                    err = Errors.wrong_data
+                    err.detail.update({"errors": output_data})
+                    raise err
 
-    @field_validator("phone")
-    @classmethod
-    def phone_match(cls, value: str):
-        if not re.match(
-            r"^(8|\+7)\d{10}$",
-            value,
-        ):
-            raise Errors.wrong_phone
-        return value
-
-    @field_validator("long_hashed_password")
-    @classmethod
-    def passwords_match(cls, value: str):
-        if not re.match(
-            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$",
-            value,
-        ):
-            raise Errors.wrong_pass
-        return value
+    # @field_validator("login")
+    # @classmethod
+    # def login_match(cls, value: str):
+    #     if not re.match(
+    #         r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{4,15}$",
+    #         value,
+    #     ):
+    #         raise Errors.wrong_login
+    #     return value
+    #
+    # @field_validator("phone")
+    # @classmethod
+    # def phone_match(cls, value: str):
+    #     if not re.match(
+    #         r"^(8|\+7)\d{10}$",
+    #         value,
+    #     ):
+    #         raise Errors.wrong_phone
+    #     return value
+    #
+    # @field_validator("long_hashed_password")
+    # @classmethod
+    # def passwords_match(cls, value: str):
+    #     if not re.match(
+    #         r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$",
+    #         value,
+    #     ):
+    #         raise Errors.wrong_pass
+    #     return value
 
     # long_hashed_password: Annotated[
     #     str,
